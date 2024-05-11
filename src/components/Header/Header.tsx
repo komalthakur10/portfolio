8f487b4536d0a1d7dd93cceabe36ef95e23a3791
komal <komalthakur100400@gmail.com>
2024-05-12 01:21:45 +0530

    header added

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,40 @@
+import { MdLightMode } from "react-icons/md";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="p-4 border-b mx-4">
+      <div className="grid lg:grid-cols-2">
+        <div className="flex">
+          <img
+            className="cursor-pointer w-12"
+            src="/assets/icons/logo.svg"
+            alt="profile-logo"
+          />
+          <div className="text-2xl pt-1 ps-4">Komal Thakur</div>
+        </div>
+        <div className="">
+          <div className="grid grid-flow-col gap-8 text-xl pt-2 lg:ms-[15rem]">
+            <div className="text-center pb-1 hover:border-b-2 hover:border-[#2c36fc]">
+              Home
+            </div>
+            <div className="text-center pb-1 hover:border-b-2 hover:border-[#2c36fc]">
+              About
+            </div>
+            <div className="text-center pb-1 hover:border-b-2 hover:border-[#2c36fc]">
+              Projects
+            </div>
+            <div className="text-center">
+              <button className="bg-gradient-to-b from-[#4f8aff] to-[#2c36fc] rounded-lg hover:shadow-md hover:shadow-[#2c36fc] hover:border-black hover:dark:border pb-1 px-4 text-white">
+                Contact
+              </button>
+            </div>
+            <div className="rounded-full bg-white hover:bg-black hover:border w-8 h-8 p-[6px] ">
+              <MdLightMode className="text-black hover:text-white" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -8,7 +8,10 @@ interface Props {
 
 const Layout = ({ children }: Props) => {
   return (
-    <div className="">
+    <div
+      className="bg-gradient-to-r from-[#1A1B54] via-[#11143F] to-[#020A20] dark:text-white"
+      // className="bg-gradient-to-r from-[#b0c4ff] via-[#7692ff] to-[#4f8aff] "
+    >
       <Header />
       <div>{children}</div>
       <Footer />
