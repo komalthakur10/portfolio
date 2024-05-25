4ca96aed315ed8bc26f87357e12ee67e612bcab6
komal <komalthakur100400@gmail.com>
2024-05-25 18:57:19 +0530

    footer ui bug fix

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
   const { dark } = useSelector((state: rootType) => state.theme);
   const themeColor = `${dark ? "[#06080c71]" : "[#15183fc1] "}`;
   return (
-    <div className="mx-10 py-12">
+    <div className="mx-4 lg:mx-10 py-12 ">
       <div
         className={`grid lg:grid-cols-3 border-2  lg:px-10 py-6 rounded-xl text-white bg-${themeColor} shadow-md  shadow-[rgba(44,54,252,0.41)] `}
       >
@@ -16,7 +16,7 @@ const Footer = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <div className="flex p-4 lg:p-0 lg:justify-center">
+          <div className="flex flex-wrap  p-4 lg:p-0 lg:justify-center">
             <IoMdMail className=" text-[#4f8aff] w-14 h-10 me-4" />
             <div className="pt-2">komalthakur100400@gmail.com</div>
           </div>
@@ -26,7 +26,7 @@ const Footer = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <div className="flex ps-7 lg:p-0 lg:justify-center">
+          <div className="flex flex-wrap  ps-7 lg:p-0 lg:justify-center">
             <SiLinkedin className=" text-[#4f8aff] w-8 h-8 mt-1 me-6" />
             <div className="pt-2 ps-1">komal-thakur-40ab06237</div>
           </div>
@@ -36,7 +36,7 @@ const Footer = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <div className="flex ps-7 pt-4 lg:p-0 lg:justify-center">
+          <div className="flex flex-wrap ps-7 pt-4 lg:p-0 lg:justify-center">
             <IoLogoGithub className=" text-[#4f8aff] w-9 h-10 " />
             <div className="pt-2 ps-6">komalthakur10</div>
           </div>
